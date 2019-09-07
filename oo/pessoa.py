b6435c9a7a2c29b433f2f0638fe0f64eed4d8779
murilocd <murilo_cd@yahoo.com>
2019-09-07 15:35:50 -0300

    Criado e removido atributo dinâmico relativo à classe Pessoa.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,3 +1,25 @@
 class Pessoa:
-    pass
+    def __init__(self, *filhos, nome, idade=36):
+        self.idade = idade
+        self.nome = nome
+        self.filhos = list(filhos)
 
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+
+if __name__ == "__main__":
+    murilo = Pessoa(nome="Murilo Pai")
+    cesar = Pessoa(murilo, nome="Cesar Filho") ## murilo é filho de cesar
+
+    print(Pessoa.cumprimentar(murilo))
+    print(cesar.nome)
+
+    for filho in cesar.filhos:
+        print(filho.nome)
+
+    cesar.sobrenome = "Drumond" ## criado atributo dinâmico
+    del cesar.filhos ##removido atributo dinâmico
+    # atributos dinâmicos = atributos criados em tempo de execução, específico para um objeto ou situação
+    print(cesar.__dict__) # método que exibe os atributos de cada objeto
+    print(murilo.__dict__)
