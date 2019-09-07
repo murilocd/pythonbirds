ef57e5294d1c50f8a3b8d7ad5e5f782bd7b73796
murilocd <murilo_cd@yahoo.com>
2019-09-07 16:13:38 -0300

    Criado atributo de classe "olhos"

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,7 @@
 class Pessoa:
+    olhos = 2 # atributo de classe, comum a todos objetos
+              ## __dict__não enxerga. Enxerga somente atributos de instância
+
     def __init__(self, *filhos, nome, idade=36):
         self.idade = idade
         self.nome = nome
@@ -21,5 +24,5 @@ if __name__ == "__main__":
     cesar.sobrenome = "Drumond" ## criado atributo dinâmico
     del cesar.filhos ##removido atributo dinâmico
     # atributos dinâmicos = atributos criados em tempo de execução, específico para um objeto ou situação
-    print(cesar.__dict__) # método que exibe os atributos de cada objeto
+    print(cesar.__dict__) # método que exibe os atributos de cada objeto (instancia)
     print(murilo.__dict__)
